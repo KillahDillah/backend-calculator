cdb37e24766c0ca7bc21c104dfa70432800bf463
Leah Miller <leahmiller1969@gmail.com>
2021-03-26 14:01:43 -0700

    working BMI calculator

--- server.js
+++ server.js
@@ -4,22 +4,30 @@ const bodyParser = require("body-parser");
 const app = express(); //set up express
 
 app.use(bodyParser.urlencoded({extended: true})); // parse data coming from HTML form. Use anytime the information from a form is needed
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 
 app.get("/", function(req, res){  // set up route
   res.sendFile(__dirname + "/index.html")
-})
+});
 
-app.post("/", function(req,res){
-  var num1 = Number(req.body.num1);
-  var num2 = Number(req.body.num2);
-  var result = num1 + num2;
+app.post("/", function(req, res){
+  res.send("whoop");
+});
 
-  res.send(result);
+app.get("/bmicalculator", function(req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req,res){
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height); //float is a decimal
+
+  var bmi = (weight / (height * height)) * 703;
+  res.send("Your BMI is " + bmi);
 });
 
 
 app.listen(3000, function() {   //spin up server
   console.log("Served!")
-})
+});
